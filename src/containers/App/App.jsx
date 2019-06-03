65e7cf706fa0484f08c5be8618af4ae06e597081
Lynne Rang <lynne.rang@gmail.com>
2019-06-02 23:57:16 -0600

    Move current data fetching to CurrentStats.

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -1,10 +1,5 @@
 import React, { Component } from 'react';
 import { Switch, Route, NavLink } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { addCurrentStats, addHistoricalStats } from '../../actions';
-import { mockCurrentStats, mockHistoricalStats } from '../../util/mockData';
-import { fetchCurrentStats, fetchHistoricalStats } from '../../util/api';
-import { cleanStats, cleanHistoricalStats } from '../../util/cleaners';
 import CurrentStats from '../../components/CurrentStats/CurrentStats';
 import Forecast from '../Forecast/Forecast';
 
@@ -12,25 +7,6 @@ import Forecast from '../Forecast/Forecast';
 export class App extends Component {
   state = {
     showNav: false
-  }
-
-  componentDidMount() {
-    this.getCurrentStats();
-    this.getHistoricalStats();
-  }
-
-  getCurrentStats = () => {
-    fetchCurrentStats()
-      .then(data => cleanStats(data[0].lastData))
-      .then(currentStats => this.props.addCurrentStats(currentStats))
-      .catch(err => console.log(err))
-  }
-
-  getHistoricalStats = () => {
-    fetchHistoricalStats()
-      .then(data => cleanHistoricalStats(data))
-      .then(historicalStats => this.props.addHistoricalStats(historicalStats))
-      .catch(err => console.log(err))
   }
 
   render() {
@@ -62,9 +38,8 @@ export class App extends Component {
             </div>
           </header>
           <main className="App-main">
-            {/* <Forecast /> */}
             <Switch>
-              <Route exact path='/' render={() => <CurrentStats currentStats={this.props.currentStats} historicalStats={this.props.historicalStats} />} />
+              <Route exact path='/' render={() => <CurrentStats />} />
               <Route exact path='/forecast' render={() => <Forecast />} />
             </Switch>
           </main>
@@ -75,14 +50,4 @@ export class App extends Component {
   }
 }
 
-export const mapStateToProps = state => ({
-  currentStats: state.currentStats,
-  historicalStats: state.historicalStats
-})
-
-export const mapDispatchToProps = dispatch => ({
-  addCurrentStats: currentStats => dispatch(addCurrentStats(currentStats)),
-  addHistoricalStats: historicalStats => dispatch(addHistoricalStats(historicalStats))
-})
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
